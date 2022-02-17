02e692766fd3a1f7a486db12bf11e2a38726d58c
zkonak <zelihaozsiray@gmail.com>
2022-02-17 14:31:25 +0100

    DockerFile

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,11 +1,10 @@
+import { userRouter } from './User';
+import { carTypeRouter } from './CarType';
+import { packageRouter } from './Package';
+import { serviceRouter } from './Service';
+import { reservationRouter } from './Reservation';
+import { paymentRouter } from './Payment';
 
-import {userRouter} from './User';
-import {carTypeRouter} from './CarType';
-import {packageRouter} from './Package';
-import {serviceRouter} from './Service';
-import {reservationRouter} from './Reservation';
-import {paymentRouter} from './Payment';
-
-const routes = [userRouter,carTypeRouter,packageRouter,serviceRouter,reservationRouter,paymentRouter];
+const routes = [userRouter, carTypeRouter, packageRouter, serviceRouter, reservationRouter, paymentRouter];
 
 export default routes;
